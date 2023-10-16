d7ce5c0cee8cd17dece40ed43f676fd46e1f5b3c
Firrizq <firrizq@gmail.com>
2023-10-16 08:01:53 +0700

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
 
 android {
     namespace = "com.wtt.muslimmedia"
-    compileSdkPreview = "UpsideDownCake"
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.wtt.muslimmedia"
         minSdk = 33
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
